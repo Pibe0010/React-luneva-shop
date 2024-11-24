4c2fd87b55229056cede6c99188d0b802a9ae362
Daniel Montero Castro <salamapatela@gmail.com>
2024-11-24 12:51:01 +0100

    Terminacion de la tabla de ordenes y y elementos css para su funcion

--- src/Components/PagesComponents/Order/DeleteOrderModal.jsx
+++ src/Components/PagesComponents/Order/DeleteOrderModal.jsx
@@ -1,3 +1,92 @@
-export const DeleteOrderModal = () => {
-  return <div>DeleteOrderModal</div>;
+import Swal from "sweetalert2";
+const URL = import.meta.env.VITE_URL;
+
+export const DeleteOrderModal = ({ onDelete, id, token }) => {
+  const handleProductDelete = async () => {
+    const confirmButtonText = onDelete ? "Eliminar" : "Cancelar";
+    const confirmButtonColor = onDelete ? "#dc3545" : "#28a745";
+    const confirmationMessage = onDelete
+      ? `¿Quieres Eliminar la orden?`
+      : `¿Quieres Cancelar la orden?`;
+
+    const confirmed = await Swal.fire({
+      title: "¿Estás seguro?",
+      text: confirmationMessage,
+      icon: "question",
+      allowOutsideClick: false,
+      showCancelButton: true,
+      confirmButtonColor: confirmButtonColor,
+      cancelButtonColor: "#6c757d",
+      confirmButtonText: `Sí, ${confirmButtonText}`,
+    });
+
+    if (confirmed.isConfirmed) {
+      try {
+        const response = await fetch(`${URL}/order/delete/${id}`, {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `${token}`,
+          },
+        });
+
+        if (!response.ok) {
+          console.error(
+            "Error en la respuesta del servidor:",
+            response.status,
+            response.statusText
+          );
+        }
+
+        if (response.ok) {
+          const responseData = await response.json();
+          console.log("Orden eliminado correctamente", responseData);
+
+          onDelete(responseData.data);
+
+          const successMessage = `Orden eliminado con éxito`;
+
+          const Toast = Swal.mixin({
+            toast: true,
+            position: "top-end",
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+              toast.onmouseenter = Swal.stopTimer;
+              toast.onmouseleave = Swal.resumeTimer;
+            },
+          });
+
+          Toast.fire({
+            icon: "success",
+            text: successMessage,
+          });
+        } else {
+          Swal.fire({
+            icon: "error",
+            text: "La orden no ha sido cancelada",
+          });
+        }
+      } catch (error) {
+        console.error("Error al realizar la operación:", error);
+        Swal.fire({
+          icon: "error",
+          text: "Hubo un problema al realizar la operación",
+        });
+      }
+    }
+  };
+
+  return (
+    <div>
+      <button className="delete-btn" onClick={handleProductDelete}>
+        <img
+          className="delete-icon"
+          src="/Icons/cancel.svg"
+          alt="Borrar producto"
+        />
+      </button>
+    </div>
+  );
 };
--- src/Components/PagesComponents/Order/OrderListTable.jsx
+++ src/Components/PagesComponents/Order/OrderListTable.jsx
@@ -1,18 +1,20 @@
 import { useUser } from "../../../Context/AutContext.jsx";
 import { DeleteOrderModal } from "./DeleteOrderModal.jsx";
 import { MoreOrder } from "./MoreOrder.jsx";
-import { StatusOrderController } from "./StatusOrderController.jsx";
-import { UpdateOrder } from "./UpdateOrder.jsx";
+/* import { StatusOrderController } from "./StatusOrderController.jsx"; */
+/* import { UpdateOrder } from "./UpdateOrder.jsx"; */
 import { GetNormalizaDate } from "../../../Services/GetNormalizaDate.js";
 import "./OrderListTable.css";
 
-export const OrderListTable = ({
-  order,
-  updateOrder,
-  deleteOrder,
-  activeOrder,
-}) => {
+export const OrderListTable = ({ order, deleteOrder }) => {
   const token = useUser();
+
+  const statusTranslations = {
+    earring: "Pendiente",
+    sent: "Enviado",
+    delivered: "Entregado",
+    cancelled: "Cancelado",
+  };
 
   return (
     <section id="customer_table" className="orderTable">
@@ -23,6 +25,7 @@ export const OrderListTable = ({
         <div className="orderTableHeadRowProduct headRow">Producto</div>
         <div className="orderTableHeadRowPrice headRow">Precio</div>
         <div className="orderTableHeadRowCuantity headRow">Cantidad</div>
+        <div className="orderTableHeadRowDiscount headRow">Descuento</div>
         <div className="orderTableHeadRowStatus headRow">Estado</div>
         <div className="orderTableHeadRowCreate headRow">Creado</div>
         <div className="orderTableHeadRowActions headRow">Acciones</div>
@@ -36,35 +39,39 @@ export const OrderListTable = ({
                 {orderItem.user_name} {orderItem.last_name}
               </div>
               <div className="orderTableBodyRowEmail">{orderItem.email}</div>
-              <div className="orderTableBodyRowProduct">
-                {orderItem.product_name}
-              </div>
-              <div className="orderTableBodyRowPrice">{orderItem.price}</div>
+              <div className="orderTableBodyRowProduct">{orderItem.name}</div>
+              <div className="orderTableBodyRowPrice">{orderItem.price} €</div>
               <div className="orderTableBodyRowCuantity">
-                {orderItem.product_amount}
+                {orderItem.product_amount} u
+              </div>
+              <div className="orderTableBodyRowDiscount">
+                {orderItem.product_discount
+                  ? `${orderItem.product_discount} €`
+                  : "0 €"}
               </div>
               <div
-                className={`orderTableBodyRowStatus ${orderItem.active ? "active" : "inactive"}`}
+                className={`orderTableBodyRowStatus ${orderItem.status.toLowerCase()}`}
               >
-                {orderItem.active ? "Activo" : "Inactivo"}
+                {statusTranslations[orderItem.status.toLowerCase()] ||
+                  orderItem.status}
               </div>
               <div className="orderTableBodyRowCreate">
                 {GetNormalizaDate(orderItem.createdAt).toLocaleDateString()}
               </div>
               <div className="orderTableBodyRowActions">
-                <MoreOrder orderr={orderItem} />
-                <StatusOrderController
+                <MoreOrder order={orderItem} />
+                {/* <StatusOrderController
                   id={orderItem.ID_order}
                   isActive={orderItem.active}
                   activeOrder={activeOrder}
                   token={token}
-                />
+                /> 
                 <UpdateOrder
                   id={orderItem.ID_order}
                   onUpdateOrder={updateOrder}
                   offerData={orderItem}
                   formTypes="order"
-                />
+                />*/}
                 <DeleteOrderModal
                   id={orderItem.ID_order}
                   onDelete={deleteOrder}
